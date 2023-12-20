83dd5fb702b21293fc89bfbf7af4af41a6c3bb4c
Rohit Kumar <rk116704@gmail.com>
2023-12-20 12:23:08 +0530

    This is my first commit

---
+++ src/main/java/com/telusko/demo/controller/AlienController.java
@@ -0,0 +1,36 @@
+package com.telusko.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.demo.model.Alien;
+import com.telusko.demo.repository.AlienRepo;
+
+@Controller
+public class AlienController {
+	@Autowired
+	AlienRepo repo;
+	
+@RequestMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+@RequestMapping("/addAlien")
+public String addAlien(Alien alien) {
+	repo.save(alien);
+	return "home.jsp";
+}
+@RequestMapping("/getAlien")
+public ModelAndView getAlien(@RequestParam int aid) {
+	ModelAndView mv=new ModelAndView("showAlien.jsp");
+	Alien alien =repo.findById(aid).orElse(new Alien());
+	mv.addObject(alien);
+	System.out.println(repo.findByTech("java"));
+	System.out.println(repo.findByAidGreaterThan(6));
+	System.out.println(repo.findByTechSorted("java"));
+	return mv;
+}
+}
